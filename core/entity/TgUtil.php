fb392a3c4d9925da7d58a3bc3ea5aa403966a9fa
Ltre Oreki <Ltre@users.noreply.github.com>
2018-12-24 20:34:13 +0800

    sendImageOrAnimateByTuku

--- core/entity/TgUtil.php
+++ core/entity/TgUtil.php
@@ -15,4 +15,34 @@ class TgUtil extends DIEntity {
         return $text;
     }
 
+
+    //发送自建图库的图片或动态图
+    static function sendImageOrAnimateByTuku($tg, array $chat, array $tuData, array $otherArgs){
+        $caption = "tuId={$tuData['tuId']}\nTags: " . join('; ', $tuData['tags']);
+        $headers = get_headers($tuData['url'], 1);
+        if (in_array($headers['Content-Type'], ['image/gif', 'video/mp4'])) {
+            return $tg->callMethod('sendVideo', [
+                'chat_id' => $chat['id'],
+                'video' => $tuData['url'],
+                'caption' => 'gif|mp4: '.$caption,
+                'reply_to_message_id' => $otherArgs['reply_to_message_id'],
+            ]);
+        } elseif (preg_match('/^image\//i', $headers['Content-Type'])) {
+            return $tg->callMethod('sendPhoto', [
+                'chat_id' => $chat['id'],
+                'photo' => $tuData['url'],
+                'caption' => $caption,
+                'reply_to_message_id' => $otherArgs['reply_to_message_id'],
+            ]);
+        } else {
+            $responseText = "Unsupported MIMETYPE：{$headers['Content-Type']}";
+            return $tg->callMethod('sendMessage', [
+                'chat_id' => $chat['id'],
+                'text' => $responseText,
+                'reply_to_message_id' => $otherArgs['reply_to_message_id'],
+                'parse_mode' => 'Markdown',
+            ]);
+        }
+    }
+
 }
